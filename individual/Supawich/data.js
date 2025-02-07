a606329adb9f6c64e24f552c142890ef4a606268
ilovefood <supawich007@gmail.com>
2025-02-07 19:31:21 +0700

    fix by ake

--- individual/Supawich/data.js
+++ individual/Supawich/data.js
@@ -1,15 +1,48 @@
 export const testimonials = [
   ];
 
-export const skills =[
-  {
-  id:1,
-  icon:"",
-  alt: "",
-  title:"",
-  level:"",
-  description:""
-},
+  export const skills = [
+    {
+        id: 1,
+        icon: "../img/html.png",
+        alt: "HTML",
+        title: "HTML",
+        level: "Beginner",
+        description: "Understands the fundamentals of structuring webpages using semantic HTML"
+    },
+    {
+        id: 2,
+        icon: "../img/css.png",
+        alt: "CSS",
+        title: "CSS",
+        level: "Beginner",
+        description: "Able to style webpages using CSS selectors, Flexbox, Grid, and responsive design techniques."
+    },
+    {
+        id: 3,
+        icon: "../img/figma.png",
+        alt: "Figma",
+        title: "Figma",
+        level: "Beginner",
+        description: "Familiar with UI/UX design principles, creating wireframes, prototypes, and collaborating on design projects."
+    },
+    {
+        id: 4,
+        icon: "../img/ai.png",
+        alt: "Illustrator",
+        title: "Illustrator",
+        level: "Beginner to Intermediate",
+        description: "Comfortable with vector design, logo creation, and basic digital illustrations using Adobe Illustrator."
+    },
+    {
+        id: 5,
+        icon: "../img/py.png",
+        alt: "Python",
+        title: "Python",
+        level: "Beginner",
+        description: "Skilled in basic Python logic, OpenCV, and simple camera model implementation."
+    }
 ];
+
 
 export const features = [];
