e5618cf254d47524897b5dd3c2c6c828ff6bdb5f
IamNatthamon <66010413@kmitl.ac.th>
2025-02-07 23:09:46 +0700

    fixed

--- individual/Natthamon/data.js
+++ individual/Natthamon/data.js
@@ -4,12 +4,45 @@ export const testimonials = [
 export const skills =[
   {
   id:1,
-  icon:"",
-  alt: "",
-  title:"",
-  level:"",
-  description:""
+  icon:"../img/html.png",
+  alt: "HTML",
+  title:"HTML",
+  level:"Beginner",
+  description:"Able to use structure, tags, and elements to create simple web pages."
 },
+{
+  id:2,
+  icon:"../img/css.png",
+  alt: "CSS",
+  title:"CSS",
+  level:"Beginner",
+  description:"Able to use selectors, properties, and styles to design simple web pages."
+},
+{
+  id:3,
+  icon:"../img/js.png",
+  alt: "JavaScript",
+  title:"JavaScript",
+  level:"Beginner",
+  description:"Able to use basic JavaScript, including variables and simple functions."
+},
+{
+  id:4,
+  icon:"../img/figma.png",
+  alt: "Figma",
+  title:"FIGMA",
+  level:"Beginner",
+  description:"Able to create simple designs, wireframes, and prototypes with essential tools."
+},
+{
+  id:5,
+  icon:"../img/py.png",
+  alt: "Python",
+  title:"Python",
+  level:"Beginner",
+  description:"Able to write simple code with variables, loops, and functions."
+},
+
 ];
 
 export const features = [];
